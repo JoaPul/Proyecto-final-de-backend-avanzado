87883b6d8b4c592e3866b65c9e6a261260ec8f8d
Joaquin Arroyo <87846400+JoaPul@users.noreply.github.com>
2022-08-29 20:23:05 -0500

    first commit

---
+++ api/api.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const api = express();
+api.use(express.json());
+
+// PRUEBA DE CONECCIÓN
+api.get('/status', (_, res) => {
+  return res.json({
+    msg: 'API funcionando',
+  })
+})
+
+export default api;
